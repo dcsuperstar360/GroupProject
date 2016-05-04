5d8793eb88d0e68b88b9014453af398b1b73d268
Alex <dcsuperstar360@gmail.com>
2016-05-04 12:30:53 -0600

    New background/other

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
 $(document).ready(function() {
 	console.log("Loaded");	
-
-var p = $('p#test');
-p.replaceWith('id="username' + p.html() + '>');
+    var Firebase = require("firebase")
+    var fbref = new Firebase("htts://social-media-tracker.firebaseio.com/");
+    
 });
-
-<input type="button" value="click me" onclick="function getConfirmation(){
-               var retVal = confirm("Do you want to continue ?");
-               if( retVal == true ){
-                  document.write ("Thank you for submting!");
-                  return true;
-               }
+    $('#social-media').submit(function(e) {
+		e.preventDefault();
+    	var totaltwitter = parseInt($('#totaltwitter').val());
+		var totalfacebook = parseInt($('#totalfacebook').val());
+		var totalinstagram = parseInt($('#totalinstagram').val());
+        var totalsnapchat = parseInt($('#totalsnapchat').val());
+        	var answer = {
+			totaltwitter: totaltwitter,
+			totalfacebook: totalfacebook,
+			totalinstagram: totalinstagram,
+                totalsnapchat: totalsnapchat
+		}
+        fbRef.child('answers').push(answer);
+    });
+var p = $('p#totalinstagram');
+p.replaceWith('id="totalinstagram' + p.html() + '>');
+});
+var p = $('p#totalfacebook');
+p.replaceWith('id="totalfacebook' + p.html() + '>');
+});
+var p = $('p#totalsnapchat');
+p.replaceWith('id="totalsnapchat' + p.html() + '>');
+});
+var p = $('p#totaltwitter');
+p.replaceWith('id="totaltwitter1' + p.html() + '>');
+});
+    	fbRef.child('answers').on('child_added', function(snap) {
+		var answer = snap.val();
+               
             }" />
             
             
